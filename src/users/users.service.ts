d9586934a935bb49a5377a3b0ecc341b3203f39d
AleksandrMenshchikov <aleksandr.menshchikov1982@gmail.com>
2024-04-13 12:17:49 +0300

    add JWT_SECRET

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { User } from './entities/user.entity';
-import { Repository } from 'typeorm';
-import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {
-  constructor(
-    @InjectRepository(User) private readonly usersRepository: Repository<User>,
-  ) {}
-}
+export class UsersService {}
